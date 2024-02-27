c85aa13d9ce78eeb8afde849df8eca886ef84545
dimpleshakrawar <dimpleshakrawar@gmail.com>
2024-02-27 17:51:47 +0530

    utils and prettier config files

---
+++ src/app.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+//for json file size limit
+app.use(express.json({ limit: "16kb" }));
+
+//using search
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+//to store images or pdf publicly
+app.use(express.static("public"));
+
+//to access cookies from server to browser and vice versa
+app.use(cookieParser());
+
+export { app };
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import { app } from "./app.js";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDB()
+  .then(() => {
+    app.listen(process.env.port || 8000, () => {
+      console.log(`Server is running at ${process.env.PORT} `);
+    });
+  })
+  .catch((err) => console.log("MONGODB connection failed !!!", err));
